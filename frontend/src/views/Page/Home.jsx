9c62b929afc1340e16873a36808438cbfac45752
Jean Ma <xintongm1@student.unimelb.edu.au>
2021-05-05 18:00:17 +0930

    new frontend
    front end with mapbox

---
+++ frontend/src/views/Page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+
+import './css/Home.css'
+
+export default function FrontPage() {
+    return (
+        <div className='welcome'>
+        </div>
+    )
+}
