c961d8f3c9f0499e4e4895e4734fff6251a09627
RubyLLL <xlyu00green@gmail.com>
2021-05-22 21:48:44 +0800

    comm with node

--- frontend/read_ini.js
+++ frontend/read_ini.js
@@ -4,12 +4,12 @@ const ini = require('ini')
 const express = require('express')
 var cors = require('cors')
 
-// const FILE = 'config/instance_ips.ini'
+const FILE = 'config/instance_ips.ini'
 
-// var config = ini.parse(fs.readFileSync(FILE, 'utf-8'))
-// const ServerObject = config.webserver
-// const ip = Object.keys(ServerObject)[0]
-const ip = '0.0.0.0'
+var config = ini.parse(fs.readFileSync(FILE, 'utf-8'))
+const ServerObject = config.webserver
+const ip = Object.keys(ServerObject)[0]
+
 const app = express()
 app.use(cors())
 app.get('/', (req, res) => {
