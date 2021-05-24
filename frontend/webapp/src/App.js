34d1240a95a668d02c12a087d4707c050d1f1f4a
Shirui Cheng <shiruic@outlook.com>
2021-05-24 13:50:58 +0000

    fix frontend

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 
 import OpenPage from './views/Page/OpenPage'
-import Home from './views/Page/Home'
 import Scenario1 from './views/Page/Scenario1'
 import Scenario2 from './views/Page/Scenario2'
-
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -12,7 +10,6 @@ function App() {
   return (
     <Router>
       <Route path="/"exact component={OpenPage} />
-      <Route path="/map"exact component={Home} />
       <Route path="/scenario-1" component={Scenario1} />
       <Route path="/scenario-2" component={Scenario2} />
     </Router>
