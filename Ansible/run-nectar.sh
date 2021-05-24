6c83bafde0644b03323809e904e06f35f73f4016
Shirui Cheng <shiruic@outlook.com>
2021-05-25 04:06:09 +1000

    ansible docker-compose

--- Ansible/run-nectar.sh
+++ Ansible/run-nectar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
-./unimelb-comp90024-2021-grp-41-openrc.sh; ansible-playbook mrc.yaml --private-key config/general.pem
 . unimelb-comp90024-2021-grp-41-openrc.sh
+./unimelb-comp90024-2021-grp-41-openrc.sh; ansible-playbook mrc.yaml --private-key config/general.pem
